c1f46b67ef17e1149ce6a2e857564aec541e6d67
naruepol <naruapon@gmail.com>
2024-04-24 12:59:45 +0700

    change content name

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>NPRU 2024</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
